c47ad7112ec0ef4cc7c4d9e982d8b3b6fee034a4
silence <beast964089@gmail.com>
2022-01-05 16:03:36 +0800

    change pie chart text

--- component/PieChart.tsx
+++ component/PieChart.tsx
@@ -4,9 +4,9 @@ const PieChart = () => {
     return (
         <div className="pie">
             <div className="pie-content">
-                {new Array(6).fill(0).map((el, idx) =>
-                    <div className="pie-part">
-                        <label className="pie-label">{idx + 1}</label>
+                {["圓", "餅", "圖", "css", "手", "刻"].map((el) =>
+                    <div className="pie-part" key={el}>
+                        <label className="pie-label">{el}</label>
                     </div>)
                 }
             </div>
